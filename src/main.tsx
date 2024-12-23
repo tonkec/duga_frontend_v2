0b6ebb5ec6acb645bf6c3ab588d6d5c1e31d54f5
tonkec <antonija1023@gmail.com>
2024-12-23 21:33:15 +0100

    remove unused styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter } from 'react-router';
