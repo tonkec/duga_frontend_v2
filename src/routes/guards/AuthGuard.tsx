71f18bf6c880bdc6813db1d4cea96cea33d94c54
tonkec <antonija1023@gmail.com>
2024-12-24 08:28:29 +0100

    add guard for homepage

---
+++ src/routes/guards/AuthGuard.tsx
@@ -0,0 +1,16 @@
+import { Navigate } from 'react-router-dom';
+import { useLocalStorage } from '@uidotdev/usehooks';
+
+interface IAuthGuardProps {
+  children: React.ReactNode;
+}
+
+export const AuthGuard = ({ children }: IAuthGuardProps) => {
+  const [isLoggedIn] = useLocalStorage('token');
+
+  if (!isLoggedIn) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,19 @@ import { Route, Routes } from 'react-router';
 import App from '../App';
 import LoginPage from '../pages/Login';
 import SignupPage from '../pages/Signup';
+import { AuthGuard } from './guards/AuthGuard';
 
 const DugaRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route
+        path="/"
+        element={
+          <AuthGuard>
+            <App />
+          </AuthGuard>
+        }
+      />
       <Route path="/login" element={<LoginPage />} />
       <Route path="signup" element={<SignupPage />} />
     </Routes>
