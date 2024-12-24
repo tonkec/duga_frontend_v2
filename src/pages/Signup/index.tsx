c502ad5d3173c94e8755a1d63b2d7e94025496fb
tonkec <antonija1023@gmail.com>
2024-12-24 07:58:05 +0100

    add signup page

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -3,22 +3,22 @@ import AuthLayout from '../../components/ui/AuthLayout';
 import Button from '../../components/ui/Button';
 import Input from '../../components/ui/Input';
 
-const LoginPage = () => {
+const SignupPage = () => {
   return (
     <AuthLayout>
       <form className="w-[400px]">
-        <h1 className="text-center">Ulogiraj se!</h1>
+        <h1 className="text-center">Prijavi se!</h1>
         <Input placeholder="Email" value="" className="mb-2 mt-2" />
         <Input placeholder="Lozinka" value="" />
         <Button onClick={() => {}} className="w-full mt-2" type="primary">
-          Ulogiraj se
+          Prijavi se
         </Button>
-        <Link to="/signup" className="text-center block mt-2 underline">
-          Registriraj se!
+        <Link to="/login" className="text-center block mt-2 underline">
+          Ulogiraj se!
         </Link>
       </form>
     </AuthLayout>
   );
 };
 
-export default LoginPage;
+export default SignupPage;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router';
 import App from '../App';
 import LoginPage from '../pages/Login';
+import SignupPage from '../pages/Signup';
 
 const DugaRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="signup" element={<SignupPage />} />
     </Routes>
   );
 };
