cb12f4ab50adfe469b13ba8e1eb03a674f61788d
tonkec <antonija1023@gmail.com>
2024-12-26 15:18:20 +0100

    add more hardcoded content

--- src/pages/MyProfilePage/index.tsx
+++ src/pages/MyProfilePage/index.tsx
@@ -13,6 +13,7 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { useState } from 'react';
 import Button from '../../components/Button';
 import { BiArrowBack } from 'react-icons/bi';
+import Iframe from 'react-iframe';
 
 interface IImage {
   createdAt: string;
@@ -67,7 +68,7 @@ const MyProfilePage = () => {
 
   return (
     <AppLayout>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-5 mb-5">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5 mb-3">
         <div className="col-span-2">
           <Card>
             <div className="flex gap-6">
@@ -81,35 +82,100 @@ const MyProfilePage = () => {
                 />
               </div>
 
-              <div>
-                <h1 className="text-3xl mb-4">
-                  {currentUser?.data.firstName} {currentUser?.data.lastName}
-                </h1>
-                <p className="flex items-center text-lg">
-                  <BiSolidMap /> {currentUser?.data.location}
-                </p>
-                <p className="flex items-center text-lg">
-                  <BiBody /> {currentUser?.data.gender}
-                </p>
-                <p className="flex items-center text-lg">
-                  <BiBoltCircle /> {currentUser?.data.sexuality}
-                </p>
-                <p className="flex items-center text-lg">
-                  <BiStopwatch /> {currentUser?.data.age} godina
-                </p>
+              <div className="flex gap-6">
+                <div className="flex flex-col gap-2">
+                  <h1 className="text-3xl mb-4">
+                    {currentUser?.data.firstName} {currentUser?.data.lastName}
+                  </h1>
+                  <p className="flex items-center text-lg gap-2">
+                    <BiSolidMap /> <b>Lokacija: </b> {currentUser?.data.location}
+                  </p>
+                  <p className="flex items-center text-lg gap-2">
+                    <BiBody /> <b>Rod: </b> {currentUser?.data.gender}
+                  </p>
+                  <p className="flex items-center text-lg gap-2">
+                    <BiBoltCircle /> <b>Seksualnost: </b> {currentUser?.data.sexuality}
+                  </p>
+                  <p className="flex items-center text-lg gap-2">
+                    <BiStopwatch /> <b>Godine: </b> {currentUser?.data.age}
+                  </p>
+                </div>
+
+                <div className="flex flex-col gap-2 mt-20">
+                  <p className="flex items-center text-lg gap-2 mt-[-8px]">
+                    <b>Tražim:</b> ozbiljnu vezu
+                  </p>
+                  <p className="flex items-center text-lg gap-2">
+                    <b>Trenutno sam: </b> Single
+                  </p>
+                  <p className="flex items-center text-lg gap-2">
+                    <b>Cigarete:</b> Ne!
+                  </p>
+                  <p className="flex items-center text-lg gap-2">
+                    <b>Alkohol:</b> Ne!
+                  </p>
+                  <p className="flex items-center text-lg gap-2">
+                    <b>Interesi: </b> Kuglanje, bitcoin
+                  </p>
+                  <p className="flex items-center text-lg gap-2">
+                    <b>Jezici koje govorim: </b> Hrvatski, Bosanski
+                  </p>
+                  <p className="flex items-center text-lg gap-2">
+                    <b>Religija:</b> Ne!
+                  </p>
+                  <p className="flex items-center text-lg gap-2">
+                    <b>Politika:</b> Apolitičnost
+                  </p>
+                  <p className="flex items-center text-lg gap-2">
+                    <b>Najdraži dan u tjednu:</b> Petak
+                  </p>
+                </div>
               </div>
             </div>
 
-            <div>
+            <div className="mb-10">
               <h2 className="font-bold mt-5">O meni</h2>
               <p>{getUserBio(currentUser?.data.bio)}</p>
+            </div>
+
+            <div className="mb-10">
+              <h2 className="font-bold mt-5">Najsramotnija stvar koja mi se dogodila</h2>
+              <p>Ulovili su me dok piškim u javnosti</p>
+            </div>
+
+            <div className="mb-10">
+              <h2 className="font-bold mt-5">Imam previše godina za...</h2>
+              <p>izlaske petkom</p>
+            </div>
+
+            <div className="mb-10">
+              <h2 className="font-bold mt-5">Dan mi je ljepši ako...</h2>
+              <p>vidim svog psa</p>
+            </div>
+
+            <div className="mb-10">
+              <h2 className="font-bold mb-5">Najdraža youtube pjesma</h2>
+              <Iframe
+                url="https://www.youtube.com/embed/iuz4Hp4-1uU?si=tDWcD5HsprpGMvY-"
+                width="600"
+                height="400"
+              />
+            </div>
+
+            <div className="mb-10">
+              <h2 className="font-bold mb-5">Trailer na najdraži film</h2>
+              <Iframe
+                url="https://www.youtube.com/embed/KnrRy6kSFF0?si=5q6UKW91IH7IG7Vh"
+                width="600"
+                height="400"
+              />
             </div>
 
             <h2 className="font-bold mt-5 mb-2">Fotografije</h2>
             <div className="flex gap-5">
               {allImagesWithoutProfilePhoto.map((image: IImage, index: number) => {
                 return (
-                  <div className="max-w-[200px]">
+                  <div className="max-w-[400px]">
                     <img
                       className="cursor-pointer"
                       src={`${REACT_APP_S3_BUCKET_URL}/${image.url}`}
@@ -128,16 +194,16 @@ const MyProfilePage = () => {
 
         <div className="col-span-1 max-w-[300px]">
           <Cta
-            buttonText="Pošalji"
-            subtitle="Možda te baš čeka zanimljiva osoba."
-            title="Pošalji nekome poruku!"
-            onClick={() => {}}
-          />
-          <Cta
-            buttonText="Uredi"
-            className="mt-4"
+            buttonText="Uredi profil"
+            className="mb-4"
             subtitle="Impresioniraj ekipu svojim profilom."
             title="Uredi svoj profil!"
+            onClick={() => {}}
+          />{' '}
+          <Cta
+            buttonText="Pošalji poruku"
+            subtitle="Možda te baš čeka zanimljiva osoba."
+            title="Pošalji nekome poruku!"
             onClick={() => {}}
           />
           <Cta
