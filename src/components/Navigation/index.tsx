ddc68c1ae753e7fb03462d3f535058162c8cd56b
tonkec <antonija1023@gmail.com>
2024-12-26 15:58:41 +0100

    improve navigation

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,5 +1,8 @@
 import { useLocalStorage } from '@uidotdev/usehooks';
 import { Link, useNavigate } from 'react-router';
+import { BiGroup } from 'react-icons/bi';
+import { BiUser } from 'react-icons/bi';
+import { BiExit } from 'react-icons/bi';
 
 const navigationStyles = 'flex justify-end space-x-4 bg-black p-4 shadow-sm text-white';
 
@@ -15,16 +18,21 @@ const Navigation = () => {
 
   return (
     <nav className={navigationStyles}>
-      <ul className="flex space-x-4">
+      <ul className="flex gap-2 space-x-4">
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/" className="flex items-center gap-1">
+            <BiGroup fontSize={25} />
+            Profili
+          </Link>
         </li>
         <li>
-          <Link to="/profile">Moj Profil</Link>
+          <Link to="/profile" className="flex items-center gap-1">
+            <BiUser fontSize={22} /> Moj Profil
+          </Link>
         </li>
         <li>
-          <span className="cursor-pointer" onClick={onLogout}>
-            Logout
+          <span className="cursor-pointer flex items-center gap-1" onClick={onLogout}>
+            <BiExit fontSize={25} /> Logout
           </span>
         </li>
       </ul>
