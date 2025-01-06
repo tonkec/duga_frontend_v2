08f16a68e695bf3bc03505349aaa9e634e99a8c1
tonkec <antonija1023@gmail.com>
2025-01-06 17:08:55 +0100

    fix spacings

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -2,17 +2,7 @@ import { BallTriangle } from 'react-loader-spinner';
 
 const Loader = () => {
   return (
-    <div
-      className="fixed 
-    top-0
-    left-0
-    w-full
-    h-full
-    flex
-    justify-center
-    items-center
-    z-50"
-    >
+    <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center z-50">
       <BallTriangle color="#F037A5" height={100} width={100} />
     </div>
   );
