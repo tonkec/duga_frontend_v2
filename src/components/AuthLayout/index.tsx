386b2df08b5dc0080252b6d13ef688d4be3694c8
tonkec <antonija1023@gmail.com>
2025-01-07 13:22:07 +0100

    fix auth layout

--- src/components/AuthLayout/index.tsx
+++ src/components/AuthLayout/index.tsx
@@ -7,16 +7,16 @@ interface IAuthLayoutProps {
 const AuthLayout = ({ children }: IAuthLayoutProps) => {
   return (
     <div className="lg:flex h-screen">
-      <div className="flex flex-col items-center justify-end w-full flex-1 bg-blue pt-5 px-5 xl:max-w-[700px]">
+      <div className="flex flex-col items-center justify-end w-full h-full flex-1 bg-blue pt-5 px-5 xl:max-w-[700px]">
         <div>
           <div className="mx-auto w-[400px] mb-64">
             <h1 className="text-white text-6xl text-center mb-6">Duga</h1>
             <h3 className="text-white text-center">Upoznaj zanimljive queer osobe s Balkana.</h3>
           </div>
-          <div className="mb-24 mt-10 w-[400px]">{children}</div>
+          <div className="mb-6 mt-10 w-[400px]">{children}</div>
         </div>
       </div>
-      <div className="self-end p-4">
+      <div className="self-end p-4 hidden lg:flex flex-1">
         <AuthLogo />
       </div>
     </div>
