98ffd3cc33ef596b165e9ba355e4372b01c779a6
tonkec <antonija1023@gmail.com>
2025-01-08 08:17:07 +0100

    add asset for not found

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,10 +1,13 @@
 import AppLayout from '../../components/AppLayout';
+import NotFoundPageSvg from '../../assets/not_found_page.svg';
 
 const NotFoundPage = () => {
   return (
     <AppLayout>
-      <h1>404</h1>
-      <p>Stranica ne postoji</p>
+      <div className="flex flex-col items-center justify-center h-full">
+        <img src={NotFoundPageSvg} alt="Not Found Page" className="w-1/2 h-1/2" />
+        <h1 className="text-2xl font-bold mt-4">Stranica ne postoji.</h1>
+      </div>
     </AppLayout>
   );
 };
