8aaac86acf0d1c8b4d09d2013f8260471a36cbb4
tonkec <antonija1023@gmail.com>
2025-01-14 17:15:50 +0100

    improve auth responsiveness

--- src/components/AuthLayout/index.tsx
+++ src/components/AuthLayout/index.tsx
@@ -6,17 +6,17 @@ interface IAuthLayoutProps {
 
 const AuthLayout = ({ children }: IAuthLayoutProps) => {
   return (
-    <div className="lg:flex h-screen px-4">
+    <div className="lg:flex h-screen">
       <div className="flex flex-col items-center justify-center w-full h-full flex-1 pt-5 px-5 xl:max-w-[500px]">
-        <div className="bg-blue rounded-lg px-5 py-10">
-          <div className="mx-auto w-[400px]">
+        <div className="bg-blue rounded-lg px-5 py-10 w-full">
+          <div className="mx-auto md:w-[400px]">
             <h1 className="text-white text-6xl text-center mb-6">Duga</h1>
             <h3 className="text-white text-center">Upoznaj zanimljive queer osobe s Balkana.</h3>
           </div>
-          <div className="mt-10 w-[300px] mx-auto">{children}</div>
+          <div className="mt-10 md:w-[300px] mx-auto">{children}</div>
         </div>
       </div>
-      <div className="p-4 hidden lg:flex flex-1 self-center">
+      <div className="p-4 hidden md:flex flex-1 self-center">
         <AuthLogo />
       </div>
     </div>
