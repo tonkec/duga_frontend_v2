d3a50f724875d0c66be330265d5923cf4ebdeb02
tonkec <antonija1023@gmail.com>
2025-01-16 18:53:44 +0100

    debounce search

--- src/pages/ChatPage/components/SendMessage/index.tsx
+++ src/pages/ChatPage/components/SendMessage/index.tsx
@@ -1,6 +1,5 @@
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import Button from '../../../../components/Button';
-import Input from '../../../../components/Input';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import FieldError from '../../../../components/FieldError';
@@ -14,6 +13,7 @@ import data from '@emoji-mart/data';
 import { SyntheticEvent, useState } from 'react';
 import { init, SearchIndex } from 'emoji-mart';
 import EmojiPicker from '../../../../components/EmojiPicker';
+import { debounce } from 'lodash';
 type Inputs = {
   content: string;
 };
@@ -21,10 +21,15 @@ type Inputs = {
 const schema = z.object({
   content: z
     .string()
-    .min(1)
-    .refine((val) => val.trim() !== '', {
-      message: 'Poruka je obavezna.',
-    }),
+    .min(1, { message: 'Poruka je obavezna.' })
+    .refine(
+      (val) => {
+        return val.trim().length > 0 || /[^\s]/.test(val);
+      },
+      {
+        message: 'Poruka je obavezna.',
+      }
+    ),
 });
 
 interface ISendMessageProps {
@@ -38,10 +43,11 @@ interface IEmoji {
   }[];
 }
 
+const inputStyles = `bg-white focus:outline-none focus:shadow-outline border border-gray-200 rounded py-[6px] px-4 pr-8 block w-full appearance-none leading-normal focus:border-blue`;
+
 const SendMessage = ({ chatId, otherUserId }: ISendMessageProps) => {
   init({ data });
   const [currentEmojis, setCurrentEmojis] = useState([]);
-  const [inputValue, setInputValue] = useState('');
   const socket = useSocket();
   const [currentUserId] = useLocalStorage('userId');
   const { userChats } = useGetAllUserChats(currentUserId as string);
@@ -49,10 +55,12 @@ const SendMessage = ({ chatId, otherUserId }: ISendMessageProps) => {
   const chat = userChats?.data?.find((chat: IChat) => Number(chat.id) === Number(chatId));
 
   const {
-    register,
     handleSubmit,
     formState: { errors, isValid },
     reset,
+    control,
+    setValue,
+    getValues,
   } = useForm<Inputs>({
     resolver: zodResolver(schema),
   });
@@ -82,47 +90,67 @@ const SendMessage = ({ chatId, otherUserId }: ISendMessageProps) => {
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Input
-          type="text"
-          placeholder="Pošalji poruku"
-          {...register('content')}
-          onChange={async (e: SyntheticEvent) => {
-            const value = (e.target as HTMLInputElement).value;
-            const emojiRegex = /(?:\s|^):([a-zA-Z0-9_]+)$/;
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name="content"
+        control={control}
+        render={({ field }) => {
+          const handleSearch = async (value: string) => {
+            const emojiRegex = /(?:\s|^):([^\s:]+)/;
             const match = value.match(emojiRegex);
+
             if (match) {
-              const emojis = await search(value.split(':')[1]);
-              if (emojis) setCurrentEmojis(emojis);
+              const searchTerm = match[1];
+              const emojis = await search(searchTerm);
+              setCurrentEmojis(emojis);
             } else {
               setCurrentEmojis([]);
             }
+          };
 
-            setInputValue(value);
-          }}
-          onFocus={() => {
-            socket.emit('typing', { chatId, userId: currentUserId, toUserId: [otherUserId] });
-          }}
-          onBlur={() => {
-            socket.emit('stop-typing', { chatId, userId: currentUserId, toUserId: [otherUserId] });
-          }}
-          value={inputValue}
-        />
+          const debouncedSearch = debounce(handleSearch, 300);
 
-        <EmojiPicker
-          onEmojiSelect={(emoji) => {
-            setInputValue(inputValue.slice(0, inputValue.lastIndexOf(':')) + emoji + ' ');
-          }}
-          emojis={currentEmojis}
-        />
+          return (
+            <input
+              className={inputStyles}
+              type="text"
+              placeholder="Pošalji poruku"
+              {...field}
+              onChange={(e: SyntheticEvent) => {
+                const value = (e.target as HTMLInputElement).value;
+                debouncedSearch(value);
+                field.onChange(e);
+              }}
+              onFocus={() => {
+                socket.emit('typing', { chatId, userId: currentUserId, toUserId: [otherUserId] });
+              }}
+              onBlur={() => {
+                socket.emit('stop-typing', {
+                  chatId,
+                  userId: currentUserId,
+                  toUserId: [otherUserId],
+                });
+              }}
+            />
+          );
+        }}
+      />
 
-        {errors.content && <FieldError message="Poruka je obavezna." />}
-        <Button className="mt-2" type="primary">
-          Pošalji
-        </Button>
-      </form>
-    </>
+      <EmojiPicker
+        emojis={currentEmojis}
+        onEmojiSelect={(emoji: string) => {
+          const currentValue = getValues('content');
+          const updatedValue = currentValue.replace(/(?:\s|^):([^\s:]+)/, emoji);
+          setValue('content', updatedValue, { shouldValidate: true });
+          setCurrentEmojis([]);
+        }}
+      />
+
+      {errors.content && <FieldError message="Poruka je obavezna." />}
+      <Button className="mt-2" type="primary">
+        Pošalji
+      </Button>
+    </form>
   );
 };
 
