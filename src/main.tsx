96256916e605e6c60cbd9bf650f463b70c34c92f
tonkec <antonija1023@gmail.com>
2025-01-24 14:52:31 +0100

    first steps with auth0

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,10 @@ import DugaRoutes from './routes/index.tsx';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { SocketProvider } from './context/SocketProvider.tsx';
+import { Auth0Provider } from '@auth0/auth0-react';
+import history from './utils/history.ts';
+import { getConfig } from './auth0Config.ts';
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -18,13 +22,36 @@ const queryClient = new QueryClient({
   },
 });
 
+const config = getConfig();
+
+type AppState = {
+  returnTo?: string;
+  [key: string]: string | undefined;
+};
+
+const onRedirectCallback = (appState: AppState | undefined) => {
+  history.push(appState && appState.returnTo ? appState.returnTo : window.location.pathname);
+};
+
+const providerConfig = {
+  domain: config.domain,
+  clientId: config.clientId,
+  onRedirectCallback,
+  authorizationParams: {
+    redirect_uri: window.location.origin,
+    ...(config.audience ? { audience: config.audience } : null),
+  },
+};
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <SocketProvider>
-          <DugaRoutes />
-        </SocketProvider>
+        <Auth0Provider {...providerConfig}>
+          <SocketProvider>
+            <DugaRoutes />
+          </SocketProvider>
+        </Auth0Provider>
         <ToastContainer />
       </BrowserRouter>
       <ReactQueryDevtools initialIsOpen={false} />
