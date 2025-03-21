def67436c1563c399086aa01af83045e551c1202
Antonija Šimić <antonija1023@gmail.com>
2025-03-21 09:22:32 +0100

    Feat/resend verification email (#14)
    * feat(resend-verification-email): post to new endpoint
    
    * feat(resend-verification-email): change env var
    
    * feat(resend-verification-email): add toast messages

--- src/pages/VerifyEmailPage/index.tsx
+++ src/pages/VerifyEmailPage/index.tsx
@@ -2,11 +2,34 @@ import { useAuth0 } from '@auth0/auth0-react';
 import { Navigate, useNavigate } from 'react-router';
 import AuthLayout from '../../components/AuthLayout';
 import Button from '../../components/Button';
+import { toast } from 'react-toastify';
+import { toastConfig } from '../../configs/toast.config';
 
 const VerifyEmailPage = () => {
   const navigate = useNavigate();
   const { user } = useAuth0();
   const isUserVerified = user?.email_verified;
+
+  const resendVerificationEmail = async () => {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_BASE_URL}/send-verification-email`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ userId: user?.sub }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+
+      if (response.ok) {
+        toast.success('E-mail je uspješno poslan.', toastConfig);
+      }
+    } catch (error) {
+      toast.error('Došlo je do greške prilikom slanja e-maila.', toastConfig);
+      console.error('Failed to resend verification email:', error);
+    }
+  };
 
   if (!user) {
     return <Navigate to="/login" />;
@@ -15,16 +38,23 @@ const VerifyEmailPage = () => {
   if (!isUserVerified) {
     return (
       <AuthLayout>
-        <h1 className="text-white"> Molimo te da verificiraš svoj e-mail </h1>
+        <h1 className="text-white">Molimo te da verificiraš svoj e-mail</h1>
         <p className="text-white mt-2">Ako želiš koristiti aplikaciju, potvrdi svoj e-mail.</p>
+
         <Button
           type="primary"
           className="w-full mt-4 py-4 rounded-xl"
-          onClick={() => {
-            navigate('/login');
-          }}
+          onClick={() => navigate('/login')}
         >
           Natrag na login
+        </Button>
+
+        <Button
+          type="secondary"
+          className="w-full mt-4 py-4 rounded-xl"
+          onClick={resendVerificationEmail}
+        >
+          Pošalji e-mail
         </Button>
       </AuthLayout>
     );
