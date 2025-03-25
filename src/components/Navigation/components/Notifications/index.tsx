3a23e839e4d72c0e678c66a8ff1a793049370cf4
tonkec <antonija1023@gmail.com>
2025-03-25 08:33:05 +0100

    fix(notifications): show no notifications

--- src/components/Navigation/components/Notifications/index.tsx
+++ src/components/Navigation/components/Notifications/index.tsx
@@ -64,7 +64,7 @@ const NotificationDropdown = ({ userId }: { userId: number | null }) => {
       {open && (
         <div className="absolute left-0 right-0 mt-2 w-52 bg-white shadow-xl rounded-lg z-10 max-h-96 overflow-y-auto">
           {notifications.length === 0 ? (
-            <div className="p-4 text-sm text-gray">No notifications</div>
+            <div className="p-4 text-sm text-black">No notifications</div>
           ) : (
             notifications.map((n) => (
               <div
