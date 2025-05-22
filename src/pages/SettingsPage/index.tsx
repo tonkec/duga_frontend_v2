fa5a97931461c39bd6406b3f53a26b216cec0969
tonkec <antonija1023@gmail.com>
2025-05-22 08:22:05 +0200

    feature(cookies-policy): move cookies to settings page

--- src/pages/SettingsPage/index.tsx
+++ src/pages/SettingsPage/index.tsx
@@ -6,6 +6,9 @@ import { useDeleteUser } from '../EditMyProfilePage/hooks';
 import { useLocalStorage } from '@uidotdev/usehooks';
 import Card from '@app/components/Card';
 import OnlineStatus from '@app/components/Navigation/components/OnlineStatus';
+import { toast } from 'react-toastify';
+import { toastConfig } from '@app/configs/toast.config';
+import { useCookies } from 'react-cookie';
 
 interface IDeleteProfileModalProp {
   isOpen: boolean;
@@ -27,9 +30,26 @@ const DeleteProfileModal = ({ isOpen, onClose, onDelete }: IDeleteProfileModalPr
 };
 const SettingsPage = () => {
   const [userId] = useLocalStorage('userId');
-
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
   const { deleteUserMutation } = useDeleteUser(userId as string);
+  const [, setCookie, removeCookie] = useCookies(['cookieAccepted', 'cookieRejectedAt']);
+
+  const acceptCookies = () => {
+    setCookie('cookieAccepted', 'true', { path: '/', maxAge: 60 * 60 * 24 * 365 });
+    removeCookie('cookieRejectedAt', { path: '/' });
+    toast.success('Kolačići su prihvaćeni.', toastConfig);
+  };
+
+  const rejectCookies = () => {
+    const now = new Date().toISOString();
+    setCookie('cookieRejectedAt', now, { path: '/', maxAge: 60 * 60 * 24 * 7 });
+    removeCookie('cookieAccepted', { path: '/' });
+
+    toast.info('Neke funkcije neće raditi jer ste odbili kolačiće.', toastConfig);
+  };
+
+  const [cookies] = useCookies(['cookieAccepted', 'cookieRejectedAt']);
+  const hasRejectedCookies = cookies.cookieRejectedAt;
 
   return (
     <AppLayout>
@@ -48,6 +68,20 @@ const SettingsPage = () => {
         <h1 className="text-2xl font-bold mt-4 mb-4">Postavke</h1>
 
         <div>{String(userId) && <OnlineStatus userId={String(userId)} />}</div>
+
+        <hr className="mb-5" />
+
+        <div className="mb-5 flex gap-2">
+          {hasRejectedCookies ? (
+            <Button type="primary" onClick={acceptCookies}>
+              Prihvati kolačiće
+            </Button>
+          ) : (
+            <Button type="primary" onClick={rejectCookies}>
+              Odbij kolačiće
+            </Button>
+          )}
+        </div>
 
         <hr className="mb-5" />
 
