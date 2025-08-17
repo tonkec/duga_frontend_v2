4276d845b7941a001cfa0348a93c03bb15b66bee
Antonija Šimić <antonija1023@gmail.com>
2025-08-17 13:32:30 +0200

    Bug/responsiveness issues (#113)
    * bug(responsiveness-issues): fix links
    
    * bug(responsiveness-issues): fix grid

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -346,7 +346,7 @@ const LoginPage = () => {
 
       <footer>
         <div className="bg-gray-800 text-white py-8">
-          <div className="flex justify-center items-center gap-2">
+          <div className="flex flex-col md:flex-row justify-center items-center gap-2">
             <div>
               <Link to="/cookie-policy" className="text-white underline">
                 Politika kolačića
