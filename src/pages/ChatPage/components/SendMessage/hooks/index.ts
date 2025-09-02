0751e71db3d6e854655618efd9aaac9ed6ce8209
tonkec <antonija1023@gmail.com>
2025-09-02 08:53:09 +0200

    feature(recognition): show error for explicit content

--- src/pages/ChatPage/components/SendMessage/hooks/index.ts
+++ src/pages/ChatPage/components/SendMessage/hooks/index.ts
@@ -1,5 +1,12 @@
 import { useMutation } from '@tanstack/react-query';
 import { uploadMessagePhotos } from '@app/api/uploads';
+import { toast } from 'react-toastify';
+import { toastConfig } from '@app/configs/toast.config';
+import { AxiosError } from 'axios';
+
+type BackendError = {
+  errors: { reason: string }[];
+};
 
 export const useUploadMessageImage = () => {
   const {
@@ -8,6 +15,13 @@ export const useUploadMessageImage = () => {
     isSuccess,
   } = useMutation({
     mutationFn: (data: FormData) => uploadMessagePhotos(data),
+    onSuccess: () => {
+      toast.success('Slika uspješno poslana', toastConfig);
+    },
+    onError: (error: AxiosError<BackendError>) => {
+      const errors = error?.response?.data?.errors;
+      toast.error(errors?.map((err: { reason: string }) => err.reason).join(' '), toastConfig);
+    },
   });
 
   return { uploadMessageImage, isError, isSuccess };
