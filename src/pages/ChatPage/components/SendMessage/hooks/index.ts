ae5c17c11aee109c49cf4b1708a06d153f0f781f
tonkec <antonija1023@gmail.com>
2025-09-02 08:53:36 +0200

    feature(recognition): remove success

--- src/pages/ChatPage/components/SendMessage/hooks/index.ts
+++ src/pages/ChatPage/components/SendMessage/hooks/index.ts
@@ -15,9 +15,6 @@ export const useUploadMessageImage = () => {
     isSuccess,
   } = useMutation({
     mutationFn: (data: FormData) => uploadMessagePhotos(data),
-    onSuccess: () => {
-      toast.success('Slika uspješno poslana', toastConfig);
-    },
     onError: (error: AxiosError<BackendError>) => {
       const errors = error?.response?.data?.errors;
       toast.error(errors?.map((err: { reason: string }) => err.reason).join(' '), toastConfig);
