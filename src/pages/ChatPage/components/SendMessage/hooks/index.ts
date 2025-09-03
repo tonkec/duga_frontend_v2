f70fe2b3e4720a0862b93c83d16c91392c3e0b18
Antonija Šimić <antonija1023@gmail.com>
2025-09-03 13:39:33 +0200

    Feature/recognition (#117)
    * feature(recognition): show error for explicit content
    
    * feature(recognition): remove success
    
    * feature(recognition): fix race condition

--- src/pages/ChatPage/components/SendMessage/hooks/index.ts
+++ src/pages/ChatPage/components/SendMessage/hooks/index.ts
@@ -1,13 +1,27 @@
 import { useMutation } from '@tanstack/react-query';
 import { uploadMessagePhotos } from '@app/api/uploads';
+import { toast } from 'react-toastify';
+import { toastConfig } from '@app/configs/toast.config';
+import { AxiosError } from 'axios';
 
-export const useUploadMessageImage = () => {
+type BackendError = {
+  errors: { reason: string }[];
+};
+
+export const useUploadMessageImage = (emitImageToSockets: () => void) => {
   const {
     mutate: uploadMessageImage,
     isError,
     isSuccess,
   } = useMutation({
     mutationFn: (data: FormData) => uploadMessagePhotos(data),
+    onSuccess: () => {
+      emitImageToSockets();
+    },
+    onError: (error: AxiosError<BackendError>) => {
+      const errors = error?.response?.data?.errors;
+      toast.error(errors?.map((err: { reason: string }) => err.reason).join(' '), toastConfig);
+    },
   });
 
   return { uploadMessageImage, isError, isSuccess };
