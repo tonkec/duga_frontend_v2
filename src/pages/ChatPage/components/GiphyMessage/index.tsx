3187f1442609db012de9ed2cf965e5d701ca6c72
tonkec <antonija1023@gmail.com>
2025-09-05 12:59:07 +0200

    bug(gifs-not-working): style gifs

--- src/pages/ChatPage/components/GiphyMessage/index.tsx
+++ src/pages/ChatPage/components/GiphyMessage/index.tsx
@@ -3,7 +3,7 @@ interface IGiphyMessageProps {
 }
 
 const GiphyMessage = ({ messagePhotoUrl }: IGiphyMessageProps) => {
-  return <img src={messagePhotoUrl} alt="gif" />;
+  return <img src={messagePhotoUrl} alt="gif" style={{ maxWidth: '100px' }} />;
 };
 
 export default GiphyMessage;
