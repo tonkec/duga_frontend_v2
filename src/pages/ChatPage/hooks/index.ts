3887b22c1e8a493a1651d7d7bf305e8e5b73d34a
Antonija Šimić <antonija1023@gmail.com>
2025-09-11 09:20:08 +0200

    Bug/delete chat via sockets (#143)
    * bug(delete-chat-via-sockets): emit new delete event
    
    * bug(delete-chat-via-sockets): remove partner id
    
    * bug(delete-chat-via-sockets): fix redirects

--- src/pages/ChatPage/hooks/index.ts
+++ src/pages/ChatPage/hooks/index.ts
@@ -3,7 +3,8 @@ import { getChatMessages } from '@app/api/chatMessages';
 import { deleteCurrentChat, getCurrentChat } from '@app/api/chats';
 import { toastConfig } from '@app/configs/toast.config';
 import { toast } from 'react-toastify';
-import { useNavigate } from 'react-router';
+import { Socket } from 'socket.io-client';
+import { DefaultEventsMap } from '@socket.io/component-emitter';
 
 export const useGetAllMessages = (chatId: string) => {
   const {
@@ -50,8 +51,10 @@ export const useGetCurrentChat = (chatId: string) => {
   return { currentChat, currentChatError, isCurrentChatLoading, isCurrentChatSuccess };
 };
 
-export const useDeleteCurrentChat = () => {
-  const navigate = useNavigate();
+export const useDeleteCurrentChat = (
+  socket: Socket<DefaultEventsMap, DefaultEventsMap>,
+  chatId: string | undefined
+) => {
   const {
     mutate: deleteChat,
     isPending: isDeletingChat,
@@ -60,8 +63,7 @@ export const useDeleteCurrentChat = () => {
   } = useMutation({
     mutationFn: ({ chatId }: { chatId: string }) => deleteCurrentChat(chatId),
     onSuccess: () => {
-      toast.success('Chat izbrisan!', toastConfig);
-      navigate('/new-chat');
+      socket.emit('deleteChat', { chatId });
     },
     onError: () => {
       toast.error('Greška! Probaj opet.', toastConfig);
