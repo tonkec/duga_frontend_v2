73e37d569afcbad254c39232b94bcc7c98cbe6f6
tonkec <antonija1023@gmail.com>
2025-09-12 15:40:59 +0200

    design(improvements): improve server err page

--- src/pages/NetworkErrorPage/index.tsx
+++ src/pages/NetworkErrorPage/index.tsx
@@ -1,5 +1,14 @@
+import { Link } from 'react-router';
+
 const NetworkErrorPage = () => {
-  return <h1 className="text-center mt-3">Izgleda da je server odustao ili nema interneta.</h1>;
+  return (
+    <div className="flex flex-col mx-auto items-center justify-center text-white h-full gradient">
+      <h1 className="text-center mt-3">Izgleda da se server srušio.</h1>
+      <Link to="/" className="underline">
+        Natrag na početnu
+      </Link>
+    </div>
+  );
 };
 
 export default NetworkErrorPage;
