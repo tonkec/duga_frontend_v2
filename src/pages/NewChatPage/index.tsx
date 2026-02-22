1ee3453c8211d8af400131693570c627dcf42da6
tonkec <antonija1023@gmail.com>
2026-02-22 09:26:26 +0100

    enchancemet(empty-chats): show chat only if there are some messages

--- src/pages/NewChatPage/index.tsx
+++ src/pages/NewChatPage/index.tsx
@@ -7,6 +7,8 @@ import { useSocket } from '@app/context/useSocket';
 import { useEffect } from 'react';
 import { toast } from 'react-toastify';
 import { toastConfig } from '@app/configs/toast.config';
+import { useGetCurrentUser } from '@app/hooks/useGetCurrentUser';
+import { IChat } from './hooks';
 
 const NewChatPage = () => {
   const socket = useSocket();
@@ -14,6 +16,20 @@ const NewChatPage = () => {
   const [cookies] = useCookies(['cookieAccepted', 'cookieRejectedAt']);
   const hasRejectedCookies = cookies.cookieRejectedAt;
   const { userChats, isUserChatsLoading, refetchUserChats } = useGetAllUserChats();
+  const { user: currentUser } = useGetCurrentUser();
+  const filteredChats =
+    userChats?.data?.filter((chat: IChat) => {
+      if (chat.Messages && chat.Messages.length > 0) return true;
+      // If chat user id matches current user id and messages are empty, show it
+      if (
+        currentUser &&
+        chat.Users.some((user) => user.id === currentUser.data.id) &&
+        (!chat.Messages || chat.Messages.length === 0)
+      )
+        return true;
+      // Otherwise, do not show
+      return false;
+    }) || [];
 
   useEffect(() => {
     socket.on('chatCreated', () => {
@@ -44,17 +60,6 @@ const NewChatPage = () => {
     );
   }
 
-  if (userChats?.data.length === 0 || !userChats?.data) {
-    return (
-      <AppLayout>
-        <div className="text-center mt-10 text-gray-500">
-          Još nisi započeo_la nijedan razgovor. Počni tako da posjetiš nečiji profil i pošalješ
-          poruku! 😊
-        </div>
-      </AppLayout>
-    );
-  }
-
   if (hasRejectedCookies) {
     return (
       <AppLayout>
@@ -68,7 +73,13 @@ const NewChatPage = () => {
 
   return (
     <AppLayout>
-      {userChats?.data.length > 0 && <AllUserChats userChats={userChats?.data} />}
+      {filteredChats.length > 0 && <AllUserChats userChats={filteredChats} />}
+      {filteredChats.length === 0 && (
+        <div className="text-center mt-10 text-gray-500">
+          Još nisi započeo_la nijedan razgovor. Počni tako da posjetiš nečiji profil i pošalješ
+          poruku! 😊
+        </div>
+      )}
     </AppLayout>
   );
 };
