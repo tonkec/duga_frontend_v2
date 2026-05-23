f4d9cce30e7c50613e1379c8d09c0fc656a0373d
tonkec <antonija1023@gmail.com>
2026-05-23 09:27:30 +0200

    chore(testing): add integrastion test for expired session shows logout / redirects to login

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,9 +3,9 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testMatch: ['<rootDir>/src/**/*.test.{ts,tsx}'],
   moduleNameMapper: {
-    '^@app/(.*)$': '<rootDir>/src/$1',
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '\\.(svg|png|jpg|jpeg|gif|webp)$': '<rootDir>/src/test/fileMock.ts',
+    '^@app/(.*)$': '<rootDir>/src/$1',
   },
   transform: {
     '^.+\\.(ts|tsx)$': [
